030214f39b8ee407ad9be4a4d39e108479637f19
EidAbdullahi <eidabdullahi10@gmail.com>
2020-11-09 01:39:38 +0300

    commot changes

--- quote-app/src/app/quote/quote.component.ts
+++ quote-app/src/app/quote/quote.component.ts
@@ -7,8 +7,8 @@ import { Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1, 'Sometimes later becomes never.Do it now.', 'Tiny buddha',"Peter",new Date(2020,8,27)),
-    new Quote(2,'Great things never come from comfort zones.','Roy T. Bennett',"Peter",new Date(2020,8,27)),
+    new Quote(1, 'None can destroy iron.', 'Tiny buddha',"Eid",new Date(2020,8,27)),
+    new Quote(2,'Great things never come from comfort zones.','Roy T. Bennett',"Eid",new Date(2020,8,27)),
     // new Quote(3,'The future belongs to those who believe in the beauty of their dreams.','Eleanor Roosevelt',"Peter",new Date(2020,8,27)),
     // new Quote(4,'Dont stop when you are tired. Stop when you are done.','Marilyne Monroe',"Peter",new Date(2020,8,27)),
     // new Quote(5,'Do something today that your future self will thank you for.','Sean Patrick Flanery',"Peter",new Date(2020,8,27)),
